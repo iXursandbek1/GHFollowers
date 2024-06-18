5d23cddc71ecea676ada2ade3ec16e244e7c334f
Xursandbek <xursandbek06082001@mail.com>
2024-06-18 13:18:29 +0500

    App Navigation Setup - TabBar & NavBar

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Xursandbek Qambaraliyev on 18/06/24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,29 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Xursandbek Qambaraliyev on 18/06/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
