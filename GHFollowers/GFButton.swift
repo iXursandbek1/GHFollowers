2fe3d8b66e6f85539e4efa85d9832a870031eef0
Xursandbek <xursandbek06082001@mail.com>
2024-06-19 09:52:43 +0500

    Custom UIButton & UITextField Search Screen UI

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,20 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Xursandbek Qambaraliyev on 18/06/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
